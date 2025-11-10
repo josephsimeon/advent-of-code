64643c42ea255bfbcd04abccd4f26038948f77ee
Joseph Simeon <josephdmsimeon@icloud.com>
2025-11-11 07:33:17 +1000

    Added a function called 'find_hash' to find a specific hash and returns how many iterations as well as unit tests

--- 2015/aoc-2015-4/src/main.rs
+++ 2015/aoc-2015-4/src/main.rs
@@ -1,3 +1,36 @@
+fn find_hash(secret_key: &str, pattern: &str) -> u64 {
+    let mut counter: u64 = 0;
+
+    'find_hash: loop {
+        let hash = md5::compute(format!("{}{}", secret_key, counter).as_bytes());
+        let hash_hex = format!("{:x}", hash);
+
+        if hash_hex.starts_with(pattern) {
+            break 'find_hash;
+        }
+
+        counter += 1;
+    }
+
+    counter
+}
+
+
 fn main() {
     println!("Hello, world!");
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn test_abcdef() {
+        assert_eq!(find_hash("abcdef", "00000"), 609043);
+    }
+
+    #[test]
+    fn test_pqrstuv() {
+        assert_eq!(find_hash("pqrstuv", "00000"), 1048970);
+    }
 }
