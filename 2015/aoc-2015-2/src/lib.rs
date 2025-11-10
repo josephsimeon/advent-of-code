586c299ef6a06a307a5075018dc77c24e5c0c5d1
Joseph Simeon <josephdmsimeon@icloud.com>
2025-11-10 13:49:24 +1000

    Added function to find the smallest area

--- 2015/aoc-2015-2/src/lib.rs
+++ 2015/aoc-2015-2/src/lib.rs
@@ -1,1 +1,18 @@
+fn find_smallest_area(areas: Vec<u32>) -> u32 {
+    let mut smallest = areas[0];
 
+    for area in areas.iter().skip(1) {
+        if smallest > *area { smallest = *area };
+    }
+
+    smallest
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+    #[test]
+    fn test_smallest_2x3x4() {
+        assert_eq!(find_smallest_area(vec![6, 8, 12]), 6);
+    }
+}
