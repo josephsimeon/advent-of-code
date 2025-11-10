1c599b27db970bd978a1df83127582109480906f
Joseph Simeon <josephdmsimeon@icloud.com>
2025-11-10 13:55:02 +1000

    Added in calculating wrapping paper function

--- 2015/aoc-2015-2/src/lib.rs
+++ 2015/aoc-2015-2/src/lib.rs
@@ -1,4 +1,15 @@
-fn find_smallest_area(areas: Vec<u32>) -> u32 {
+pub fn calculate_wrapping_paper(length: u32, width: u32, height: u32) -> u32 {
+    let mut area: Vec<u32> = Vec::new();
+
+    area.push(length * width);
+    area.push(width * height);
+    area.push(height * length);
+
+    let smallest = find_smallest_area(&area);
+
+    area.iter().map(|area| area * 2).sum::<u32>() + smallest
+}
+
 fn find_smallest_area(areas: &Vec<u32>) -> u32 {
     let mut smallest = areas[0];
 
@@ -12,6 +23,12 @@ fn find_smallest_area(areas: &Vec<u32>) -> u32 {
 #[cfg(test)]
 mod test {
     use super::*;
+
+    #[test]
+    fn test_2x3x4() {
+        assert_eq!(calculate_wrapping_paper(2, 3, 4), 58);
+    }
+ 
     #[test]
     fn test_smallest_2x3x4() {
         let test: Vec<u32> = vec![6, 8, 12];
