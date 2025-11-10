1ab05a35e72c38b01d2b87b5152ad35ce64e7038
Joseph Simeon <josephdmsimeon@icloud.com>
2025-11-10 16:59:25 +1000

    Added in a constructor for HouseLocation

--- 2015/aoc-2015-3/src/lib.rs
+++ 2015/aoc-2015-3/src/lib.rs
@@ -2,7 +2,11 @@
 pub struct HouseLocation(i32, i32);
 
 impl HouseLocation {
-    fn move_direction(&mut self, direction: u8) -> &Self {
+    pub fn new() -> HouseLocation {
+        HouseLocation(0, 0)
+    }
+
+    pub fn move_direction(&mut self, direction: u8) -> &Self {
         match direction {
             b'^' => self.1 += 1,
             b'>' => self.0 += 1,
@@ -20,29 +24,35 @@ mod test {
     use super::*;
 
     #[test]
+    fn test_new() {
+        let test = HouseLocation::new();
+        assert_eq!(test, HouseLocation(0, 0));
+    }
+
+    #[test]
     fn test_move_north_by_1() {
-        let mut test = HouseLocation(0, 0);
+        let mut test = HouseLocation::new();
         test.move_direction(b'^');
         assert_eq!(test, HouseLocation(0, 1));
     }
 
     #[test]
     fn test_move_east_by_1() {
-        let mut test = HouseLocation(0, 0);
+        let mut test = HouseLocation::new();
         test.move_direction(b'>');
         assert_eq!(test, HouseLocation(1, 0));
     }
 
     #[test]
     fn test_move_south_by_1() {
-        let mut test = HouseLocation(0, 0);
+        let mut test = HouseLocation::new();
         test.move_direction(b'v');
         assert_eq!(test, HouseLocation(0, -1));
     }
 
     #[test]
     fn test_move_west_by_1() {
-        let mut test = HouseLocation(0, 0);
+        let mut test = HouseLocation::new();
         test.move_direction(b'<');
         assert_eq!(test, HouseLocation(-1, 0));
     }
