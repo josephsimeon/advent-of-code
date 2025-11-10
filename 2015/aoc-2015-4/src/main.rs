7a570b7b1e113198150eb2d6531357595b21fa8a
Joseph Simeon <josephdmsimeon@icloud.com>
2025-11-11 07:35:50 +1000

    Added the solution for 2015 4

--- 2015/aoc-2015-4/src/main.rs
+++ 2015/aoc-2015-4/src/main.rs
@@ -17,7 +17,7 @@ fn find_hash(secret_key: &str, pattern: &str) -> u64 {
 
 
 fn main() {
-    println!("{}", find_hash("yzbqklnj", "00000"));
+    println!("{}, {}", find_hash("yzbqklnj", "00000"), find_hash("yzbqklnj", "000000"));
 }
 
 #[cfg(test)]
