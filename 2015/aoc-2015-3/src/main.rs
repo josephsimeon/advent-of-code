5d7393e241ed1d9963775701400940e6a50bd4e6
Joseph Simeon <josephdmsimeon@icloud.com>
2025-11-11 05:28:43 +1000

    Updated tests in 'src/main.rs'

--- 2015/aoc-2015-3/src/main.rs
+++ 2015/aoc-2015-3/src/main.rs
@@ -12,20 +12,23 @@ mod test {
     #[test]
     fn test_east_by_1() {
         let mut houses: HashMap<(i32, i32), u32> = HashMap::new();
+
         let mut house = (0, 0);
+        houses.entry(house).and_modify(|presents| *presents += 1).or_insert(1);
 
         house.move_direction('>');
         houses.entry(house).and_modify(|presents| *presents += 1).or_insert(1);
 
         let test = houses.get(&(1, 0)).unwrap();
         assert_eq!(*test, 1);
+        assert_eq!(houses.len(), 2);
     }
 
     #[test]
     fn test_turn_left() {
         let mut houses: HashMap<(i32, i32), u32> = HashMap::new();
-        let mut house = (0, 0);
-        
+
+        let mut house = (0, 0); 
         houses.entry(house).and_modify(|presents| *presents += 1).or_insert(1);
 
         for ch in "^>v<".chars() {
@@ -35,5 +38,6 @@ mod test {
 
         let test = houses.get(&(0, 0)).unwrap();
         assert_eq!(*test, 2);
+        assert_eq!(houses.len(), 4);
     }
 }
