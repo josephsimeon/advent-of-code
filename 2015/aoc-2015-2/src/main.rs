991b69fda0d3f873a67f290a44339006ebc4a186
Joseph Simeon <josephdmsimeon@icloud.com>
2025-11-10 14:21:07 +1000

    Added in a function to convert each line of input as well as a unit test

--- 2015/aoc-2015-2/src/main.rs
+++ 2015/aoc-2015-2/src/main.rs
@@ -1,3 +1,22 @@
+fn collect_digits_from_str(slice: &str) -> Vec<u32> {
+    let conversion: Vec<u32> = slice
+        .split('x')
+        .filter_map(|digit| digit.parse::<u32>().ok())
+        .collect();
+
+    conversion
+}
+
 fn main() {
     println!("Hello, world!");
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn test_2x3x4_conversion() {
+        assert_eq!(collect_digits_from_str("2x3x4"), vec![2, 3, 4]);
+    }
 }
