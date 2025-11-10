3f5ee7e3e68f2b189422d02a816bb788446e2b9b
Joseph Simeon <josephdmsimeon@icloud.com>
2025-11-11 08:45:23 +1000

    Added in a function to check for double letters in 2015 5

--- 2015/aoc-2015-5/src/lib.rs
+++ 2015/aoc-2015-5/src/lib.rs
@@ -15,14 +15,38 @@ fn does_string_contain_vowels(string: &str, num: u32) -> bool {
     false
 }
 
+fn does_string_contain_double_letters(string: &str, num: u32) -> bool {
+    let mut counter = 0;
+
+    let mut old_ch = string.chars().next().unwrap();
+    for ch in string.chars().skip(1) {
+        if old_ch == ch { counter += 1 };
+        old_ch = ch;
+    }
+
+    if counter >= num {
+        return true;
+    }
+
+    false
+}
+
 #[cfg(test)]
 mod test {
     use super::*;
+
+    #[test]
+    fn test_jchzalrnumimnmhp() {
+        let test: &str = "jchzalrnumimnmhp";
+        let bad: Vec<&str> = vec!["ab", "cd", "pq", "xy"];
+        assert_eq!(does_string_contain_double_letters(test, 1), false);
+    }
 
     #[test]
     fn test_haegwjzuvuyypxyu() {
         let test: &str = "haegwjzuvuyypxyu";
         let bad: Vec<&str> = vec!["ab", "cd", "pq", "xy"];
         assert_eq!(does_string_contain_vowels(test, 3), true);
+        assert_eq!(does_string_contain_double_letters(test, 1), true);
     }
 }
