b470f9fbdc201962080995551bdfbcffacde7fe4
Joseph Simeon <josephdmsimeon@icloud.com>
2025-11-10 14:23:28 +1000

    Added another test in 'src/main.rs'

--- 2015/aoc-2015-2/src/main.rs
+++ 2015/aoc-2015-2/src/main.rs
@@ -19,4 +19,10 @@ mod test {
     fn test_2x3x4_conversion() {
         assert_eq!(collect_digits_from_str("2x3x4"), vec![2, 3, 4]);
     }
+
+    #[test]
+    fn test_2x3x4() {
+        let test: Vec<u32> = collect_digits_from_str("2x3x4");
+        assert_eq!(calculate_wrapping_paper(test[0], test[1], test[2]), 58);
+    }
 }
