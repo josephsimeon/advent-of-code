dbd8f191edc92dbf36ae3b5a9c56621c80b50751
Joseph Simeon <josephdmsimeon@icloud.com>
2025-11-10 17:43:09 +1000

    Added some starting tests to src/main.rs and which some alterations had happened in src/lib.rs

--- 2015/aoc-2015-3/src/lib.rs
+++ 2015/aoc-2015-3/src/lib.rs
@@ -1,14 +1,14 @@
-trait Location {
-    fn move_direction(&mut self, direction: u8) -> &Self;
+pub trait Location {
+    fn move_direction(&mut self, direction: char) -> &Self;
 }
 
 impl Location for (i32, i32) {
-    fn move_direction(&mut self, direction: u8) -> &Self {
+    fn move_direction(&mut self, direction: char) -> &Self {
         match direction {
-            b'^' => self.1 += 1,
-            b'>' => self.0 += 1,
-            b'v' => self.1 -= 1,
-            b'<' => self.0 -= 1,
+            '^' => self.1 += 1,
+            '>' => self.0 += 1,
+            'v' => self.1 -= 1,
+            '<' => self.0 -= 1,
             _ => (),
         }
 
@@ -23,28 +23,28 @@ mod test {
     #[test]
     fn test_move_north_by_1() {
         let mut test = (0, 0);
-        test.move_direction(b'^');
+        test.move_direction('^');
         assert_eq!(test, (0, 1));
     }
 
     #[test]
     fn test_move_east_by_1() {
         let mut test = (0, 0);
-        test.move_direction(b'>');
+        test.move_direction('>');
         assert_eq!(test, (1, 0));
     }
 
     #[test]
     fn test_move_south_by_1() {
         let mut test = (0, 0);
-        test.move_direction(b'v');
+        test.move_direction('v');
         assert_eq!(test, (0, -1));
     }
 
     #[test]
     fn test_move_west_by_1() {
         let mut test = (0, 0);
-        test.move_direction(b'<');
+        test.move_direction('<');
         assert_eq!(test, (-1, 0));
     }
 }
--- 2015/aoc-2015-3/src/main.rs
+++ 2015/aoc-2015-3/src/main.rs
@@ -1,3 +1,39 @@
+use std::collections::HashMap;
+use aoc_2015_3::Location;
+
 fn main() {
     println!("Hello, world!");
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn test_east_by_1() {
+        let mut houses: HashMap<(i32, i32), u32> = HashMap::new();
+        let mut house = (0, 0);
+
+        house.move_direction('>');
+        houses.entry(house).and_modify(|presents| *presents += 1).or_insert(1);
+
+        let test = houses.get(&(1, 0)).unwrap();
+        assert_eq!(*test, 1);
+    }
+
+    #[test]
+    fn test_turn_left() {
+        let mut houses: HashMap<(i32, i32), u32> = HashMap::new();
+        let mut house = (0, 0);
+        
+        houses.entry(house).and_modify(|presents| *presents += 1).or_insert(1);
+
+        for ch in "^>v<".chars() {
+            house.move_direction(ch);
+            houses.entry(house).and_modify(|presents| *presents += 1).or_insert(1);
+        }
+
+        let test = houses.get(&(0, 0)).unwrap();
+        assert_eq!(*test, 2);
+    }
 }
