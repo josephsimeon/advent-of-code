4d378c94cecba06e0c173306feb36cf31f03538b
Joseph Simeon <josephdmsimeon@icloud.com>
2025-11-10 17:16:43 +1000

    Moved from using a struct to using a trait instead

--- 2015/aoc-2015-3/src/lib.rs
+++ 2015/aoc-2015-3/src/lib.rs
@@ -1,12 +1,9 @@
-#[derive(Debug, PartialEq)]
-pub struct HouseLocation(i32, i32);
+trait Location {
+    fn move_direction(&mut self, direction: u8) -> &Self;
+}
 
-impl HouseLocation {
-    pub fn new() -> HouseLocation {
-        HouseLocation(0, 0)
-    }
-
-    pub fn move_direction(&mut self, direction: u8) -> &Self {
+impl Location for (i32, i32) {
+    fn move_direction(&mut self, direction: u8) -> &Self {
         match direction {
             b'^' => self.1 += 1,
             b'>' => self.0 += 1,
@@ -24,36 +21,30 @@ mod test {
     use super::*;
 
     #[test]
-    fn test_new() {
-        let test = HouseLocation::new();
-        assert_eq!(test, HouseLocation(0, 0));
-    }
-
-    #[test]
     fn test_move_north_by_1() {
-        let mut test = HouseLocation::new();
+        let mut test = (0, 0);
         test.move_direction(b'^');
-        assert_eq!(test, HouseLocation(0, 1));
+        assert_eq!(test, (0, 1));
     }
 
     #[test]
     fn test_move_east_by_1() {
-        let mut test = HouseLocation::new();
+        let mut test = (0, 0);
         test.move_direction(b'>');
-        assert_eq!(test, HouseLocation(1, 0));
+        assert_eq!(test, (1, 0));
     }
 
     #[test]
     fn test_move_south_by_1() {
-        let mut test = HouseLocation::new();
+        let mut test = (0, 0);
         test.move_direction(b'v');
-        assert_eq!(test, HouseLocation(0, -1));
+        assert_eq!(test, (0, -1));
     }
 
     #[test]
     fn test_move_west_by_1() {
-        let mut test = HouseLocation::new();
+        let mut test = (0, 0);
         test.move_direction(b'<');
-        assert_eq!(test, HouseLocation(-1, 0));
+        assert_eq!(test, (-1, 0));
     }
 }
