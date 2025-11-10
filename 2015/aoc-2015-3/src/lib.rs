a50397fe306b7c9aa04453388665c716616b408e
Joseph Simeon <josephdmsimeon@icloud.com>
2025-11-10 16:55:25 +1000

    Added the struct to track position of location based on x,y cartes as well as a function that will move to the next location based on the input char along with unit tests

---
+++ 2015/aoc-2015-3/src/lib.rs
@@ -0,0 +1,49 @@
+#[derive(Debug, PartialEq)]
+pub struct HouseLocation(i32, i32);
+
+impl HouseLocation {
+    fn move_direction(&mut self, direction: u8) -> &Self {
+        match direction {
+            b'^' => self.1 += 1,
+            b'>' => self.0 += 1,
+            b'v' => self.1 -= 1,
+            b'<' => self.0 -= 1,
+            _ => (),
+        }
+
+        self
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn test_move_north_by_1() {
+        let mut test = HouseLocation(0, 0);
+        test.move_direction(b'^');
+        assert_eq!(test, HouseLocation(0, 1));
+    }
+
+    #[test]
+    fn test_move_east_by_1() {
+        let mut test = HouseLocation(0, 0);
+        test.move_direction(b'>');
+        assert_eq!(test, HouseLocation(1, 0));
+    }
+
+    #[test]
+    fn test_move_south_by_1() {
+        let mut test = HouseLocation(0, 0);
+        test.move_direction(b'v');
+        assert_eq!(test, HouseLocation(0, -1));
+    }
+
+    #[test]
+    fn test_move_west_by_1() {
+        let mut test = HouseLocation(0, 0);
+        test.move_direction(b'<');
+        assert_eq!(test, HouseLocation(-1, 0));
+    }
+}
