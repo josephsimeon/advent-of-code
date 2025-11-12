b36ae0d37797d07c5d0cd839d179ed30c8bf6d99
Joseph Simeon <josephdmsimeon@icloud.com>
2025-11-12 15:07:37 +1000

    Solution for 2015 10

--- 2015/aoc-2015-10/src/main.rs
+++ 2015/aoc-2015-10/src/main.rs
@@ -1,6 +1,5 @@
-fn look_and_say(num: usize) -> usize {
-    let binding = num.to_string();
-    let mut num_iter = binding.chars().peekable();
+fn look_and_say(num: &String) -> String {
+    let mut num_iter = num.chars().peekable();
     let mut say: String = String::new();
 
     let mut count = 1;
@@ -17,11 +16,23 @@ fn look_and_say(num: usize) -> usize {
         count = 1;
     }
 
-    say.parse::<usize>().unwrap()
+    say
 }
 
 fn main() {
-    println!("Hello, world!");
+    let mut say: String = "1321131112".to_string();
+
+    for _ in 0..40 {
+        say = look_and_say(&say);
+    }
+
+    print!("{}", say.len());
+
+    for i in 0..10 {
+        say = look_and_say(&say);
+    }
+
+    println!(", {}", say.len());
 }
 
 #[cfg(test)]
@@ -30,41 +41,42 @@ mod test {
 
     #[test]
     fn test_1_to_11() {
-        let test = 1;
-        assert_eq!(look_and_say(test), 11);
+        let test: String = "1".to_string();
+        assert_eq!(look_and_say(&test), "11");
     }
 
     #[test]
     fn test_11_to_21() {
-        let test = 11;
-        assert_eq!(look_and_say(test), 21);
+        let test: String = "11".to_string();
+        assert_eq!(look_and_say(&test), "21");
     }
 
     #[test]
     fn test_21_to_1211() {
-        let test = 21;
-        assert_eq!(look_and_say(test), 1211);
+        let test: String = "21".to_string();
+        assert_eq!(look_and_say(&test), "1211");
     }
 
     #[test]
     fn test_1211_to_111221() {
-        let test = 1211;
-        assert_eq!(look_and_say(test), 111221);
+        let test: String = "1211".to_string();
+        assert_eq!(look_and_say(&test), "111221");
     }
 
     #[test]
     fn test_111221_to_312211() {
-        let test = 111221;
-        assert_eq!(look_and_say(test), 312211);
+        let test: String = "111221".to_string();
+        assert_eq!(look_and_say(&test), "312211");
     }
 
     #[test]
     fn test_1_to_312211() {
-        let mut test = 1;
+        let mut test: String = "1".to_string();
+
         for _ in 0..4 {
-            test = look_and_say(test);
+            test = look_and_say(&test);
         }
 
-        assert_eq!(look_and_say(test), 312211);
+        assert_eq!(look_and_say(&test), "312211");
     }
 }
