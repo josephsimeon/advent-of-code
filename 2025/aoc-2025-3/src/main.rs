fd89d618c1ffa81feb9aadf464f959a3257df06a
Joseph Simeon <josephdmsimeon@icloud.com>
2025-12-09 09:16:08 +1000

    Moved the working code into a single function that accepts a `&str`

--- 2025/aoc-2025-3/src/main.rs
+++ 2025/aoc-2025-3/src/main.rs
@@ -2,8 +2,56 @@ fn main() {
     println!("Hello, world!");
 }
 
+fn find_total_joltage(input: &str) -> u32 {
+    let mut largest_joltage: Vec<u32> = Vec::new();
+
+    for line in input.lines() {
+        let mut largest_number = 0;
+
+        let mut first = 0;
+        for i in 0..(line.len() - 1) {
+            let parsed = line
+                .chars()
+                .nth(i)
+                .expect("could not get char")
+                .to_string()
+                .parse::<u8>()
+                .expect("Could not parse digit");
+
+            if first < parsed {
+                first = parsed;
+                let mut second = 0;
+                for j in (i + 1)..line.len() {
+                let parsed = line
+                    .chars()
+                    .nth(j)
+                    .expect("could not get char")
+                    .to_string()
+                    .parse::<u8>()
+                    .expect("Could not parse digit");
+
+                    if second < parsed {
+                        second = parsed;
+
+                        let number = first * 10 + second;
+                        if largest_number < number {
+                            largest_number = number;
+                        }
+                    }
+                }
+            }
+        }
+
+        largest_joltage.push(largest_number as u32);
+    }
+
+    largest_joltage.iter().sum::<u32>()
+}
+
 #[cfg(test)]
 mod test {
+    use super::*;
+
     static TEST_PUZZLE: &str = "987654321111111
 811111111111119
 234234234234278
@@ -11,50 +59,6 @@ mod test {
 
     #[test]
     fn test() {
-        let mut largest_joltage: Vec<u32> = Vec::new();
-        let test: u32;
-
-        for line in TEST_PUZZLE.lines() {
-            let mut largest_number = 0;
-
-            let mut first = 0;
-            for i in 0..(line.len() - 1) {
-                let parsed = line
-                    .chars()
-                    .nth(i)
-                    .expect("could not get char")
-                    .to_string()
-                    .parse::<u8>()
-                    .expect("Could not parse digit");
-
-                if first < parsed {
-                    first = parsed;
-                    let mut second = 0;
-                    for j in (i + 1)..line.len() {
-                    let parsed = line
-                        .chars()
-                        .nth(j)
-                        .expect("could not get char")
-                        .to_string()
-                        .parse::<u8>()
-                        .expect("Could not parse digit");
-
-                        if second < parsed {
-                            second = parsed;
-
-                            let number = first * 10 + second;
-                            if largest_number < number {
-                                largest_number = number;
-                            }
-                        }
-                    }
-                }
-            }
-
-            largest_joltage.push(largest_number as u32);
-        }
-
-        test = largest_joltage.iter().sum::<u32>();
-        assert_eq!(test, 357);
+        assert_eq!(find_total_joltage(TEST_PUZZLE), 357);
     }
 }
